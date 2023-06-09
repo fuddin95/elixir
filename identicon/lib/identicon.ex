f377efe9ddc382c4e62de8a0251533df49868574
user <user@spider-2.local>
2023-06-09 09:27:54 -0400

    Building Identicon, mirroring row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -13,7 +13,7 @@ defmodule Identicon do
 
   """
   def main(input) do
-    input |> hash |> pick_color |> build_grid
+    input |> hash |> pick_color |> build_grid |> filter_squares()
   end
 
   def hash(input) do
@@ -32,8 +32,26 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+  def mirror_row(row) do
+    [a, b | _tail] = row
+    row ++ [b, a]
+  end
+
   def build_grid(%Identicon.Image{hex: hex} = image) do
     #  Enum.chunk(hex, 3)
-    hex |> Enum.chunk(3)
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
+
+    %Identicon.Image{image | grid: grid}
   end
 end
