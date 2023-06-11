d4cbb4fa2543bced23a9419b8baa3dd9827f2e26
user <user@spider-2.local>
2023-06-11 04:48:12 -0400

    building Identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -13,7 +13,7 @@ defmodule Identicon do
 
   """
   def main(input) do
-    input |> hash |> pick_color |> build_grid |> filter_squares()
+    input |> hash |> pick_color |> build_grid |> filter_odd_squares() |> build_pixel_map()
   end
 
   def hash(input) do
@@ -49,9 +49,24 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
-  def filter_squares(%Identicon.Image{grid: grid} = image) do
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
